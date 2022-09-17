14c540c7a7d04a68f3c9f2199fdbddbbd910e839
Akari Neukirch <mnn1262@rit.edu>
2022-09-17 12:10:21 -0400

    Initial commit, imported DBC and created build script

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+VIRTUALENV_DIR=virtualenv
+DBC_FILENAME=formula_dbc
+
+# Create virtualenv if doesn't exist
+if [ ! -d "$VIRTUALENV_DIR" ];
+then
+	echo "Creating virtualenv"
+	python3 -m venv $VIRTUALENV_DIR
+fi
+source $VIRTUALENV_DIR/bin/activate
+
+echo "Updating Requirements"
+pip3 install -U pip wheel cantools > /dev/null
+
+echo "Building $DBC_FILENAME"
+cantools generate_c_source $DBC_FILENAME.dbc
