498eaac5f6aa957e6f2ee0169a7b950ea2dd027a
Akari Neukirch <mnn1262@rit.edu>
2024-03-23 11:14:37 -0400

    Added PCAN symbol file generation

--- update.sh
+++ update.sh
@@ -12,7 +12,12 @@ fi
 source $VIRTUALENV_DIR/bin/activate
 
 echo "Updating Requirements"
-pip3 install -U pip wheel cantools > /dev/null
+pip3 install -U pip wheel cantools canmatrix > /dev/null
 
 echo "Building $DBC_FILENAME"
 cantools generate_c_source $DBC_FILENAME.dbc
+
+echo "Building sym"
+grep -v "SystemSignalLongSymbol" $DBC_FILENAME.dbc > $DBC_FILENAME.tmp.dbc
+canconvert $DBC_FILENAME.tmp.dbc $DBC_FILENAME.sym
+rm $DBC_FILENAME.tmp.dbc
